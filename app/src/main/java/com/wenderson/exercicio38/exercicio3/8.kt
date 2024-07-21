bd531f1c90c05c59ced71f2dcdb39cd786cb8ce6
WenderBaia <138619456+WenderBaia@users.noreply.github.com>
2024-07-21 15:31:06 -0300

    Exercicio 3.8
    Neste commit foi feito as duas primeiras tarefas

---
+++ app/src/main/java/com/wenderson/exercicio38/exercicio3/8.kt
@@ -0,0 +1,17 @@
+package com.wenderson.exercicio38.exercicio3
+
+class exercicio {
+}
+
+fun main() {
+    //exercicio 1
+  /*  var numero = 6
+        numero = 5
+    println(numero)*/
+
+    //exercicio 2
+    val numero = 6
+        numero = 7
+    println(numero)
+  //O que aconteceu? Não se pode alterar o valor de uma variável imutável.
+}
